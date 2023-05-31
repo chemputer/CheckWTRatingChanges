1d358f1c3fd3da501daabae45cc965c598b41d3c
Chemputer <46601333+chemputer@users.noreply.github.com>
2023-05-31 00:26:06 -0400

    got it confirmed working, got console players nicknames instead of email placeholder

--- scrape.py
+++ scrape.py
@@ -5,16 +5,24 @@ import requests
 from bs4 import BeautifulSoup
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import re
+
+def decode_cf_email(cf_email):
+    parts = []
+    r = int(cf_email[:2], 16)
+    for i in range(2, len(cf_email), 2):
+        char_code = int(cf_email[i:i+2], 16) ^ r
+        parts.append(chr(char_code))
+    return ''.join(parts)
+
 
 # Set up Google Sheets API credentials
 credentials = ServiceAccountCredentials.from_json_keyfile_name('sa-key.json', ['https://www.googleapis.com/auth/spreadsheets'])
 client = gspread.authorize(credentials)
 
-
 spreadsheet_id = '1hPILjBhLC-Ov_3Xnhki4WDtpKdBzbvD6t56b3OzS-Gs'
 # Open the Google Sheet by its title
 sheet = client.open_by_key(spreadsheet_id).sheet1  # Replace 'sheet1' with the name of your worksheet
-
 
 url = 'https://warthunder.com/en/community/claninfo/Angels%20Of%20Death'  # Replace with the actual URL of the webpage
 response = requests.get(url)
@@ -23,7 +31,6 @@ print(html_content)
 soup = BeautifulSoup(html_content, 'html.parser')
 
 # Find the table using a CSS selector
-# Find the container element that holds the grid items using a CSS selector
 container = soup.select_one('.squadrons-members__table')
 print(container)
 # Extract the individual grid items
@@ -35,27 +42,36 @@ rows = []
 current_row = []
 
 for item in grid_items:
-    text = item.get_text(strip=True)
-    
-    # Skip empty items
-    if not text:
-        continue
-    
-    # Assign headers
-    if len(current_row) < len(headers):
-        current_row.append(text)
+    nickname_element = item.select_one('span.__cf_email__')
+    if nickname_element:
+        nickname = nickname_element['data-cfemail']
+        nickname = decode_cf_email(nickname)
+        current_row.append(nickname)
     else:
+        text = item.get_text(strip=True)
+        current_row.append(text)
+    
+    if len(current_row) == len(headers):
         rows.append(current_row)
-        current_row = [text]
+        current_row = []
 
-# Append the last row
-rows.append(current_row)
+# Clear the existing content in the sheet
+sheet.clear()
 
-# Add headers as the first row
-#rows.insert(0, headers)
+# Paste the values to the sheet
+# header_range = sheet.range('A1:F1')  # Update the range to the first row of headers
+# for i in range(len(header_range)):
+#     header_range[i].value = headers[i]
 
-# Add the data to the Google Sheet
-sheet.clear()  # Clear the existing content in the sheet
-sheet.insert_rows(rows)
+# sheet.update_cells(header_range)
+
+data_range = sheet.range('A1:F{}'.format(len(rows) + 1))  # Adjust the range to start from the first row
+for i in range(len(rows)):
+    for j in range(len(rows[i])):
+        data_range[i * len(headers) + j].value = rows[i][j]
+
+sheet.update_cells(data_range)
+# Freeze the first row
+sheet.frozen_rows = 1
 
 print("Data added to Google Sheet.")
